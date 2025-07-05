980d08b164292ad5ea4ab042d9911dd3dd7a1420
Beksultan <sharshenbekov.tech@gmail.com>
2025-07-05 13:11:38 +0600

    Создал модель гостевой ниги и создал записи и сохранил в фикстуру

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from webapp.models import Book
+
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'text', 'created_at', 'updated_at', 'statuses')
+
+
+admin.site.register(Book, BookAdmin)
+
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+statuses = [("active", "Активно"), ("blocked", "Заблокировано")]
+
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Имя")
+    email = models.EmailField(null=False, blank=False, verbose_name="Почта")
+    text = models.TextField(null=False, blank=False, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования")
+    statuses = models.CharField(max_length=20, choices=statuses, default="active", verbose_name="Статус")
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
+    class Meta:
+        db_table = 'books'
+        verbose_name = 'Сообщение'
+        verbose_name_plural = "Сообщения"
