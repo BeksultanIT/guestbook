52c31a67762e37a77a98ba4c17cd248cb75269cd
Beksultan <sharshenbekov.tech@gmail.com>
2025-07-05 13:42:15 +0600

    Создал главную страниу и базовый шалон.

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from webapp.models import Book
+
+
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.order_by('-created_at').filter(statuses='active')
+    return render(request, 'index.html', {'books': books})
